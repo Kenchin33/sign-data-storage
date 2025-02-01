baa0b96707a71fede124cdc3c256feddff991f5d
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-02-02 01:26:09 +0200

    Fixed icons css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../styles/footer.css';
-import instagramLogo from '../img/instagram.png'
+import instagramLogo from '../img/instagram_icon.png'
 import telegramLogo from '../img/telegram.png'
 
 const Footer = () => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <div className='logo'>
                 <img src={logo} alt='Funko Hunter Logo'></img>
             </div>
-            <h1 className='site-tittle'>Funko Hunter</h1>
+            <h1 className='site-title'>Funko Hunter</h1>
             <div className='instagram-header-link'>
                 <a href='https://www.instagram.com/funko.hunter/' target='_blank' rel='noopener noreferrer'>
                     <img src={instagramLogo} alt='Instagram'></img>
