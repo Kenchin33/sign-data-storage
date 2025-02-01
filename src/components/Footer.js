7097d5289534990037b3b2f8aa4abdf148dbe9ba
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-02-02 01:09:52 +0200

    Fix Photos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,20 @@
 import React from "react";
 import '../styles/footer.css';
+import instagramLogo from '../img/instagram.png'
+import telegramLogo from '../img/telegram.png'
 
 const Footer = () => {
     return(
         <footer className="footer">
             <div className='instagram-footer-link'>
                 <a href='https://www.instagram.com/funko.hunter/' target='_blank' rel='noopener noreferrer'>
-                    <img src='../img/instagram.png' alt='Instagram'></img>
+                    <img src={instagramLogo} alt='Instagram'></img>
                 </a>
             </div>
             <p>&#169; Funko Hunter</p>
             <div className='telegram-footer-link'>
                 <a href='https://t.me/funkohunter' target='_blank' rel='noopener noreferrer'>
-                    <img src='../img/telegram.png' alt='Telegram'></img>
+                    <img src={telegramLogo} alt='Telegram'></img>
                 </a>
             </div>
         </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../styles/header.css';
+import logo from '../img/logo.png'
+import instagramLogo from '../img/instagram.png'
 
 const Header = () => {
     return(
         <header className='header'>
             <div className='logo'>
-                <img src='../img/logo.png' alt='Funko Hunter Logo'></img>
+                <img src={logo} alt='Funko Hunter Logo'></img>
             </div>
             <h1 className='site-tittle'>Funko Hunter</h1>
             <div className='instagram-header-link'>
                 <a href='https://www.instagram.com/funko.hunter/' target='_blank' rel='noopener noreferrer'>
-                    <img src='../img/instagram.png' alt='Instagram'></img>
+                    <img src={instagramLogo} alt='Instagram'></img>
                 </a>
             </div>
         </header>
