b8aa292bc11db2a0d3515c5791923bce19bcb017
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-02-02 13:43:45 +0200

    Added new item to database

--- src/api/database.js
+++ src/api/database.js
@@ -1,5 +1,18 @@
 const database = [
     {
+        code: 'HS0001',
+        name: 'Signed Homer Simpson Funko Pop',
+        actor: 'Малуха Євген',
+        figureType: 'Funko Pop',
+        figureName: 'Homer in Hedges 1252',
+        exclusive: 'Entertainment Earth',
+        certificationCode: 'HS0001',
+        signatureDate: '2024-09-11',
+        phrase: 'Спроба - перший крок до провалу.',
+        imageUrl1: '/img/hs1.jpg',
+        imageUrl2: '/img/hs2.jpg',
+    },
+    {
         code: 'HS0002',
         name: 'Signed Homer Simpson Funko Pop',
         actor: 'Малуха Євген',
@@ -7,7 +20,7 @@ const database = [
         figureName: 'Mr. Plow 910',
         exclusive: 'Hot Topic',
         certificationCode: 'HS0002',
-        signatureDate: '2024-08-25',
+        signatureDate: '2024-09-11',
         phrase: 'Ще один день на роботі - ще одна вкрадена ручка',
         imageUrl1: '/img/mrplow1.jpg',
         imageUrl2: '/img/mrplow2.jpg',
